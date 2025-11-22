30e1ca0cb775348b6abc2d657cabd5f2f5db965e
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2025-11-22 10:04:54 +0530

    22-11-2025

---
+++ 03.R
@@ -0,0 +1,41 @@
+x <- 10
+
+print(class(x))
+
+y <- 10L
+
+print(class(y))
+
+z <- TRUE
+print(class(z))
+
+p <- 'Aptech'
+print(class(p))
+
+
+q <- 10+10i
+print(class(q))
+
+x <- 10.5
+y <- 100
+z = -10
+
+class(x)
+class(y)
+class(z)
+
+
+x <- 10
+class(x)
+
+x <- 10L
+class(x)
+
+
+x <- 10i
+class(x)
+
+
+y <- 1-1i
+class(y)
+
---
+++ 04.R
@@ -0,0 +1,13 @@
+print(min(c(2, 3, 4, 1)))
+
+
+print(max(c(2, 3, 4, 1)))
+
+
+print(sqrt(4))
+
+print(floor(2.14))
+
+print(ceiling(2.3))
+
+
